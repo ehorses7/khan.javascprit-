b169555b545503b45fca43fb8ed0f3176631cb1d
ehorses7 <57732688+ehorses7@users.noreply.github.com>
2019-11-13 16:27:46 -0700

    Create Start.js
    My first attempt at getting khan academy code to work.

---
+++ .github/workflows/Start.js
@@ -0,0 +1,10 @@
+fill(0, 0, 0);
+ellipse(200, 200, 375, 375);
+fill(60, 0, 255);
+triangle(200, 104, 280, 280, 120, 280);
+fill(255, 255, 255);
+var answer = floor(random(1, 5));
+if (answer > 0) {
+    text("YOUR", 176, 200);
+    text("MESSAGE", 159, 229); 
+}
